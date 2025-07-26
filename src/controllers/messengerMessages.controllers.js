f3d68b5083ef6ea21dbd262b49130dc1301ea4a6
upvisor <admin@upvisor.cl>
2025-07-25 20:45:24 -0400

    actualization general

--- src/controllers/messengerMessages.controllers.js
+++ src/controllers/messengerMessages.controllers.js
@@ -142,7 +142,7 @@ export const MessengerToken = async (req, res) => {
     }
 
     const shopLogin = await ShopLogin.findOne({ type: 'Administrador' }).lean()
-    await axios.post(`${process.env.MAIN_API_URL}/user`, { email: shopLogin.email, api: process.env.NEXT_PUBLIC_API_URL, idPage: pageId });
+    await axios.post(`${process.env.MAIN_API_URL}/user`, { email: shopLogin.email, api: process.env.API_URL, idPage: pageId });
 
     res.status(200).json({ success: 'OK' });
   } catch (err) {
