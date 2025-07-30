960060f8c547009183a47cb5c9b64de3c05849a6
upvisor <admin@upvisor.cl>
2025-07-29 23:39:14 -0400

    actualization general

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -1,12 +1,10 @@
 import {Router} from 'express'
-import { createWebhook, getMessage, callbackFacebook } from '../controllers/webhook.controllers.js'
+import { createWebhook, getMessage } from '../controllers/webhook.controllers.js'
 
 const router = Router()
 
 router.get('/webhook', createWebhook)
 
 router.post('/webhook', getMessage)
-
-router.get('/auth/facebook/callback', callbackFacebook)
 
 export default router
