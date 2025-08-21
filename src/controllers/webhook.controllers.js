63446ee4981df1130361aaf8e71ccf58319a755d
upvisor <admin@upvisor.cl>
2025-08-21 16:23:41 -0400

    actualization general

--- src/controllers/webhook.controllers.js
+++ src/controllers/webhook.controllers.js
@@ -924,6 +924,6 @@ export const getMessage = async (req, res) => {
             return res.sendStatus(200)
         }
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.sendStatus(200)
     }
 }
