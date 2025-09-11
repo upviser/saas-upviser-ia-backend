e0cb874d6ebc16ee07ca6ca1bf290825625206ab
upvisor <admin@upvisor.cl>
2025-09-11 19:17:32 -0300

    actualization general

--- src/controllers/domain.controllers.js
+++ src/controllers/domain.controllers.js
@@ -1,5 +1,6 @@
 import { Vercel } from '@vercel/sdk';
 import Domain from '../models/Domain.js'
+import axios from 'axios'
 
 export const editDomain = async (req, res) => {
     const vercel = new Vercel({
@@ -14,16 +15,31 @@ export const editDomain = async (req, res) => {
       },
     })
 
+    const brevoDomain = await axios.post(
+      "https://api.brevo.com/v3/senders/domains",
+      { domain: req.body.domain },
+      { headers: { "api-key": process.env.BREVO_API } }
+    );
+
+    await axios.post(
+      "https://api.brevo.com/v3/senders",
+      {
+        name: req.body.name,
+        email: `${req.body.email}@${req.body.domain}`,
+      },
+      { headers: { "api-key": process.env.BREVO_API } }
+    );
+
     if (mainDomainResponse.verified) {
-      const domainUpdate = await Domain.findOneAndUpdate({}, { domain: req.body.domain }, { new: true })
+      const domainUpdate = await Domain.findOneAndUpdate({}, { domain: req.body.domain, name: req.body.name, email: req.body.email, dkim1: { type: brevoDomain.data.dns_records.dkim1Record.type, value: brevoDomain.data.dns_records.dkim1Record.value, hostname: brevoDomain.data.dns_records.dkim1Record.host_name }, dkim2: { type: brevoDomain.data.dns_records.dkim2Record.type, value: brevoDomain.data.dns_records.dkim2Record.value, hostname: brevoDomain.data.dns_records.dkim2Record.host_name }, brevo: { type: brevoDomain.data.dns_records.brevo_code.type, value: brevoDomain.data.dns_records.brevo_code.value, hostname: brevoDomain.data.dns_records.brevo_code.host_name }, dmarc: { type: brevoDomain.data.dns_records.dmarc_record.type, value: brevoDomain.data.dns_records.dmarc_record.value, hostname: brevoDomain.data.dns_records.dmarc_record.host_name } }, { new: true })
 
       if (!domainUpdate) {
-          const newDomain = new Domain({ domain: req.body.domain })
+          const newDomain = new Domain({ domain: req.body.domain, name: req.body.name, email: req.body.email, dkim1: { type: brevoDomain.data.dns_records.dkim1Record.type, value: brevoDomain.data.dns_records.dkim1Record.value, hostname: brevoDomain.data.dns_records.dkim1Record.host_name }, dkim2: { type: brevoDomain.data.dns_records.dkim2Record.type, value: brevoDomain.data.dns_records.dkim2Record.value, hostname: brevoDomain.data.dns_records.dkim2Record.host_name }, brevo: { type: brevoDomain.data.dns_records.brevo_code.type, value: brevoDomain.data.dns_records.brevo_code.value, hostname: brevoDomain.data.dns_records.brevo_code.host_name }, dmarc: { type: brevoDomain.data.dns_records.dmarc_record.type, value: brevoDomain.data.dns_records.dmarc_record.value, hostname: brevoDomain.data.dns_records.dmarc_record.host_name } })
           await newDomain.save()
       }
     }
 
-    return res.json(mainDomainResponse)
+    return res.json({ domain: req.body.domain, name: req.body.name, email: req.body.email, dkim1: { type: brevoDomain.data.dns_records.dkim1Record.type, value: brevoDomain.data.dns_records.dkim1Record.value, hostname: brevoDomain.data.dns_records.dkim1Record.host_name }, dkim2: { type: brevoDomain.data.dns_records.dkim2Record.type, value: brevoDomain.data.dns_records.dkim2Record.value, hostname: brevoDomain.data.dns_records.dkim2Record.host_name }, brevo: { type: brevoDomain.data.dns_records.brevo_code.type, value: brevoDomain.data.dns_records.brevo_code.value, hostname: brevoDomain.data.dns_records.brevo_code.host_name }, dmarc: { type: brevoDomain.data.dns_records.dmarc_record.type, value: brevoDomain.data.dns_records.dmarc_record.value, hostname: brevoDomain.data.dns_records.dmarc_record.host_name } })
   } catch (error) {
     console.error(
       error instanceof Error ? `Error: ${error.message}` : String(error),
--- src/models/Domain.js
+++ src/models/Domain.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose'
 
 const DomainSchema = new mongoose.Schema({
-    domain: { type: String, required: true }
+    domain: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    dkim1: { type: { type: String }, value: { type: String }, hostname: { type: String } },
+    dkim2: { type: { type: String }, value: { type: String }, hostname: { type: String } },
+    brevo: { type: { type: String }, value: { type: String }, hostname: { type: String } },
+    dmarc: { type: { type: String }, value: { type: String }, hostname: { type: String } }
 }, {
     timestamps: true
 })
