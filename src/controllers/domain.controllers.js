057b387b6ed760e73ea2228b0854195108650f08
upvisor <admin@upvisor.cl>
2025-10-14 14:51:57 -0300

    actualization general

--- src/controllers/domain.controllers.js
+++ src/controllers/domain.controllers.js
@@ -43,15 +43,15 @@ export const editDomain = async (req, res) => {
           const newDomain = new Domain({ tenantId: req.body.tenantId, domain: req.body.domain, name: req.body.name, email: req.body.email, dkim1: { type: brevoDomain.data.dns_records.dkim1Record.type, value: brevoDomain.data.dns_records.dkim1Record.value, hostname: brevoDomain.data.dns_records.dkim1Record.host_name }, dkim2: { type: brevoDomain.data.dns_records.dkim2Record.type, value: brevoDomain.data.dns_records.dkim2Record.value, hostname: brevoDomain.data.dns_records.dkim2Record.host_name }, brevo: { type: brevoDomain.data.dns_records.brevo_code.type, value: brevoDomain.data.dns_records.brevo_code.value, hostname: brevoDomain.data.dns_records.brevo_code.host_name }, dmarc: { type: brevoDomain.data.dns_records.dmarc_record.type, value: brevoDomain.data.dns_records.dmarc_record.value, hostname: brevoDomain.data.dns_records.dmarc_record.host_name } })
           await newDomain.save()
       }
+      return res.json({ ...req.body, dkim1: { type: brevoDomain.data.dns_records.dkim1Record.type, value: brevoDomain.data.dns_records.dkim1Record.value, hostname: brevoDomain.data.dns_records.dkim1Record.host_name }, dkim2: { type: brevoDomain.data.dns_records.dkim2Record.type, value: brevoDomain.data.dns_records.dkim2Record.value, hostname: brevoDomain.data.dns_records.dkim2Record.host_name }, brevo: { type: brevoDomain.data.dns_records.brevo_code.type, value: brevoDomain.data.dns_records.brevo_code.value, hostname: brevoDomain.data.dns_records.brevo_code.host_name }, dmarc: { type: brevoDomain.data.dns_records.dmarc_record.type, value: brevoDomain.data.dns_records.dmarc_record.value, hostname: brevoDomain.data.dns_records.dmarc_record.host_name } })
     } else {
       const domainUpdate = await Domain.findOneAndUpdate({ tenantId: req.body.tenantId }, { domain: req.body.domain, name: req.body.name, email: req.body.email }, { new: true })
       if (!domainUpdate) {
           const newDomain = new Domain({ tenantId: req.body.tenantId, domain: req.body.domain, name: req.body.name, email: req.body.email })
           await newDomain.save()
       }
+      return res.json(req.body)
     }
-
-    return res.json({ ...req.body, dkim1: { type: brevoDomain.data.dns_records.dkim1Record.type, value: brevoDomain.data.dns_records.dkim1Record.value, hostname: brevoDomain.data.dns_records.dkim1Record.host_name }, dkim2: { type: brevoDomain.data.dns_records.dkim2Record.type, value: brevoDomain.data.dns_records.dkim2Record.value, hostname: brevoDomain.data.dns_records.dkim2Record.host_name }, brevo: { type: brevoDomain.data.dns_records.brevo_code.type, value: brevoDomain.data.dns_records.brevo_code.value, hostname: brevoDomain.data.dns_records.brevo_code.host_name }, dmarc: { type: brevoDomain.data.dns_records.dmarc_record.type, value: brevoDomain.data.dns_records.dmarc_record.value, hostname: brevoDomain.data.dns_records.dmarc_record.host_name } })
   } catch (error) {
     console.error(
       error instanceof Error ? `Error: ${error.message}` : String(error),
